0e495048ea11a69470d88b5f28ec610018a20072
nev <gh@f-m.fm>
2020-10-24 01:45:31 -0700

    exportssssssss

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
-export * from './date.js';
-export * from './array.js';
-export * from './promise.js';
-export * from './string.js';
+import * as DateImport from "./date.js";
+import * as ArrayImport from "./array.js";
+import * as PromiseImport from "./promise.js";
+import * as StringImport from "./string.js";
+export declare const Date: typeof DateImport;
+export declare const Array: typeof ArrayImport;
+export declare const Promise: typeof PromiseImport;
+export declare const String: typeof StringImport;
--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-export * from './date.js';
-export * from './array.js';
-export * from './promise.js';
-export * from './string.js';
+import * as DateImport from "./date.js";
+import * as ArrayImport from "./array.js";
+import * as PromiseImport from "./promise.js";
+import * as StringImport from "./string.js";
+
+export const Date = DateImport;
+export const Array = ArrayImport;
+export const Promise = PromiseImport;
+export const String = StringImport;
